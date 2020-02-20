50632b965601fc1bbc9a86f1f92549f082575f71
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-20 13:31:30 -0700

    Add mulitiple doors for main.py

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ def main():
     room_blueprints = [
         {'name': 'Kitchen',
          'desc': 'A dank and dirty room buzzing with flies.',
-         'door': {'direction': 'South', 'room': 'Dining Hall'}},
+         'doors': [{'direction': 'south', 'room': 'Dining Hall'}]
+         },
         {'name': 'Ballroom',
          'desc': 'An elegant space that has obviously seen better days.',
-         'door': {'direction': 'East', 'room': 'Dining Hall'}},
+         'doors': [{'direction': 'east', 'room': 'Dining Hall'}]
+         },
         {'name': 'Dining Hall',
          'desc': 'Broken and mismatched chairs sit around a dusty table.',
-         'door': {'direction': 'West', 'room': 'Ballroom'}}
+         'doors': [{'direction': 'west', 'room': 'Ballroom'},
+                   {'direction': 'north', 'room': 'Kitchen'}]
+         }
     ]
 
     locations = {}
 
     for room in room_blueprints:
         create_room(room, locations)
+
+    locations["Dining Hall"].get_details()
 
 
 if __name__ == "__main__":
