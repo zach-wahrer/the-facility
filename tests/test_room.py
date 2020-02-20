5a561d922ae40ddfbfe7bb97090fd8218daeb162
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-20 11:50:00 -0700

    Add testing for the Room class

---
+++ tests/test_room.py
@@ -0,0 +1,34 @@
+"""Testing for the Room class."""
+
+from helpers.room import Room, create_room
+import unittest
+
+room_blueprint = {'name': 'Kitchen',
+                  'desc': 'A dank and dirty room buzzing with flies.',
+                  'door': {'direction': 'South', 'room': 'Dining Hall'}
+                  }
+
+
+class TestRoom(unittest.TestCase):
+
+    def test_room_name(self):
+        location = {}
+        create_room(room_blueprint, location)
+        self.assertEqual(location['Kitchen'].get_name(), 'Kitchen')
+
+    def test_room_desc(self):
+        location = {}
+        create_room(room_blueprint, location)
+        description = 'A dank and dirty room buzzing with flies.'
+        self.assertEqual(location['Kitchen'].get_description(), description)
+
+    def test_door_linking(self):
+        location = {}
+        create_room(room_blueprint, location)
+        direction = 'South'
+        room = 'Dining Hall'
+        self.assertEqual(location['Kitchen'].linked_rooms[direction], room)
+
+
+if __name__ == "__main__":
+    unittest.main()
