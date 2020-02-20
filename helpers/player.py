9c7e74e80522ca2faaf7d22dfa91286e468b8efe
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-20 14:31:32 -0700

    Add Player class with movement ability

---
+++ helpers/player.py
@@ -0,0 +1,17 @@
+class Player():
+    def __init__(self, name, start_location):
+        self._name = name
+        self._location = start_location
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    def move(self, to_location, locations):
+        if to_location in self._location.linked_rooms:
+            self._location = locations[self._location.linked_rooms[to_location]]
+        else:
+            print("\nThere is no door in that direction.")
+        return self._location
