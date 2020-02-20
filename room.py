da6da022324a07f139fd42c25645688944f9a2d5
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-20 11:06:23 -0700

    Add class for rooms

---
+++ room.py
@@ -0,0 +1,23 @@
+class Room():
+    def __init__(self, name=None, description=None):
+        self._name = name
+        self._description = description
+        self.linked_rooms = {}
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, room_name):
+        self._name = room_name
+
+    def get_description(self):
+        return self._description
+
+    def set_description(self, room_description):
+        self._description = room_description
+
+    def link_room(self, room_to_link, direction):
+        self.linked_rooms[direction] = room_to_link
+
+    def describe(self):
+        print(self.get_description())
