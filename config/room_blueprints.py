bd20336a2ec0f54ef6c0a8ee4816a16ed0ecbdb8
Zach Wahrer <zachtheclimber@gmail.com>
2020-02-20 17:10:27 -0700

    Add room_blueprints.py config file

---
+++ config/room_blueprints.py
@@ -0,0 +1,15 @@
+ROOM_BLUEPRINTS = [
+    {'name': 'Kitchen',
+     'desc': 'A dank and dirty room buzzing with flies.',
+     'doors': [{'direction': 'south', 'room': 'Dining Hall'}]
+     },
+    {'name': 'Ballroom',
+     'desc': 'An elegant space that has obviously seen better days.',
+     'doors': [{'direction': 'east', 'room': 'Dining Hall'}]
+     },
+    {'name': 'Dining Hall',
+     'desc': 'Broken and mismatched chairs sit around a dusty table.',
+     'doors': [{'direction': 'west', 'room': 'Ballroom'},
+               {'direction': 'north', 'room': 'Kitchen'}]
+     }
+]
